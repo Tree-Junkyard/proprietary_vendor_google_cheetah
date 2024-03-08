6165e9b8f971574da164246ec686a27baa80ad9e
ugly-kid-af <dwarmachine24@gmail.com>
2024-03-08 13:49:58 +0530

    cheetah: Drop DeviceIntelligenceNetworkPrebuilt
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- cheetah-vendor.mk
+++ cheetah-vendor.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2022 \
     DiagMon \
     EuiccGoogle \
